e4a9ef6f61877a128fc37a74e8b4513a3533cbe4
Gunnar-Oeh <gunnaroehmichen@posteo.de>
2023-07-09 16:31:32 +0200

    03 - task 02 - Transform Data local

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,41 +4,24 @@ from time import time
 import os
 from google.cloud import storage
 
-@click.command() # click commands instead of argparse.ArgumentParser()... or sys.argv[n]
+@click.command()
 @click.option('--sa_path', help='Path to the service account json file')
-@click.option('--project_id', help='Project ID of you GCP project')
+@click.option('--project_id', help='Project ID of your GCP project')
 @click.option('--year', default=2021, help='Year to download')
-@click.option('--bucket', help='Name of the bucket to upload the data')
+@click.option('--bucket', help='Name of the bucket to download the data from')
 @click.option('--color', help='Str of the taxi-color for which data should be extracted')
+@click.option('--output_dir', help='Directory to save the downloaded data')
 
-def data_ingestion(sa_path, project_id, year, bucket, color):
-    # sa_path is the local path to the .json with the key-credentials of the Storage-Account 
-    # (who is loggin in with which role)
-    # project_id relates to the identifier of the google-cloud project
-    # year which year to download
-    # bucket: Where to store - the name of the GCS-bucket
-    
+def data_retrieval(sa_path, project_id, year, bucket, color, output_dir):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = sa_path
+    client = storage.Client()
+    bucket = client.get_bucket(bucket)
+
     for month in range(1, 4):
-        url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-{month:02d}.parquet"
-        
-        # string of the file to be stored
         file_name = f"{color}_tripdata_{year}-{month:02d}.parquet"
+        blob = bucket.blob(f'ny_taxi/{file_name}')
+        blob.download_to_filename(os.path.join(output_dir, file_name))
+        print(f"Downloaded {file_name} from GCS")
 
-        print("Loading data from url...")
-        df_taxi = pd.read_parquet(url)
-        print('Uploading data to GCS...')
-        # GC storage account key
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = sa_path
-        # Create an instance of the GCS client to communicate with the Cloud
-        client = storage.Client()
-        # Retrieve address/path to the specified bucket, where data should be stored
-        bucket = client.get_bucket(bucket)
-        
-        # Create empty blob file, before upload_... is used to transfer the parquet as string to the cloud into the blob
-        bucket.blob(f'ny_taxi/{file_name}').\
-            upload_from_string(df_taxi.to_parquet(), 'text/parquet')
-        print("Successfully uploaded the data!")
-
-### All function parameters are passed via the command line
 if __name__ == '__main__':
-    data_ingestion()
+    data_retrieval()
--- src/revenue_report_local.py
+++ src/revenue_report_local.py
@@ -18,6 +18,11 @@ import pyspark
 
 ### 3. E: Extract the data as a function
 def extract_data(sa_path, bucket, color, year, month):
+    # Create Spark Session
+    spark = SparkSession.builder \
+    .master("local") \
+    .appName(f"Pipe-{color}_taxi_{year}-{month:02d}") \
+    .getOrCreate()
     # string of the file to be loaded
     file_name = f"ny_taxi/{color}_tripdata_{year}-{month:02d}.parquet"
     
@@ -34,20 +39,51 @@ def extract_data(sa_path, bucket, color, year, month):
     pq_taxi = blob.download_as_bytes()    
     pq_taxi = BytesIO(pq_taxi)
     
-    # read the object in memory
+    # read the object in memory as df -> spark-df
     df_taxi = pd.read_parquet(pq_taxi)
-    return df_taxi
+    df_taxi.drop("ehail_fee", inplace=True, axis=1)
+    df_taxi = spark.createDataFrame(df_taxi)
+
+    return df_taxi, spark
+
+### 4. Repartition the data
+def repartition_data():
+    pass
 
 ### 4. T: Transform with Spark-SQL as a function 
     ### First just get one row
-def transform_data(df_taxi):
+def transform_data(df_taxi, spark):
     # some sql commands
-    pass
-    # return df_transformed
+    df_taxi = df_taxi \
+        .withColumnRenamed('lpep_pickup_datetime', 'pickup_datetime') \
+        .withColumnRenamed('lpep_dropoff_datetime', 'dropoff_datetime')\
+        .withColumnRenamed('PULocationID', 'pickup_location_id')\
+        .withColumnRenamed('DOLocationID', 'dropoff_location_id')
+    # Temporary SQL Table to be queried
+    df_taxi.registerTempTable('df_taxi_temp')
+    # Query the revenue
+    df_result = spark.sql("""
+                    SELECT pickup_location_id AS revenue_zone,
+                    date_trunc('day', pickup_datetime) AS revenue_day,
+                    date_trunc('month', pickup_datetime) AS month,  
+                    SUM(fare_amount) AS revenue_daily_fare,
+                    SUM(extra) AS revenue_daily_extra,
+                    SUM(mta_tax) AS revenue_daily_mta_tax,
+                    SUM(tip_amount) AS revenue_daily_tip_amount,
+                    SUM(tolls_amount) AS revenue_daily_tolls_amount,
+                    SUM(improvement_surcharge) AS revenue_daily_improvement_surcharge,
+                    SUM(total_amount) AS revenue_daily_total_amount,
+                    SUM(congestion_surcharge) AS revenue_daily_congestion_surcharge,
+                    AVG(passenger_count) AS avg_daily_passenger_count,
+                    AVG(trip_distance) AS avg_daily_trip_distance
+                    FROM df_taxi_temp
+                    GROUP BY revenue_zone, revenue_day, month;
+                      """)
+    return df_result
 
-### 5. L: Load Data onto local Machine
+### 5. L: Load Data onto local Machine PostgreSQL
 def load_data(df_transformed):
-    # some commands to write it into storage as some file
+    # some commands to write it into storage in the db
     pass
 
 ### Main with print to test the script running
